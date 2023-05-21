549b84f70f9dc04e51d7b834b8937ac5ea227064
Yuri Merkury <yg@trueflip.io>
2023-05-21 21:55:24 +0300

    added games

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CreateGroupsSeeder::class);
-        $this->call(CreateRoomItemssSeeder::class);
+//        $this->call(CreateRoomItemssSeeder::class);
     }
 }
